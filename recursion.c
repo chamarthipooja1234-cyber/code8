2f05884aff285ce55eebab70e7ca9ad3dd9875ef
chamarthipooja1234-cyber <chamarthipooja1234@gmail.com>
2025-11-29 11:36:56 +0530

    Add files via upload

---
+++ recursion.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+long long factorial(int n)
+{
+return 1;
+else
+return n*factorial-1);
+}
+int main()
+{
+int num;
+printf("enter a number:");
+scanf("%d",&num);
+if(num<0)
+printf("factorial  not defined for negative numbers\n");
+else
+printf("factorial of%d is %||d\n",num,factorial(num));
+
+return 0;
+}
